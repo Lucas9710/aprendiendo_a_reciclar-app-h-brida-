868c41195b300c27ed2920177aac7244194a4f94
LucasN <lucasnunez9710@gmail.com>
2020-01-08 18:07:50 -0300

    escribiendo codigo

---
+++ aprendiendoAreciclar/js/juego.js
@@ -0,0 +1,13 @@
+var segundos = 0;
+var resultado = 0;
+var vuelta = 0;
+const Cant_vueltas = 0;
+var reloj;
+
+
+// calcula el ancho de la barra de resultado
+
+var puntos = 10;
+const Ancho_maximo = 107;
+
+console.log(puntos *  Ancho_maximo / 100);
