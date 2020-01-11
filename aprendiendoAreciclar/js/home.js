5aa92ee89cddb31723db6b4b2556462e0848a181
LucasN <lucasnunez9710@gmail.com>
2020-01-11 19:09:24 -0300

    home

---
+++ aprendiendoAreciclar/js/home.js
@@ -0,0 +1,16 @@
+// al cargar la página…”
+
+var nivel = "";
+var segundos;
+
+switch(nivel){
+    case "facil": segundos = 9;
+    break;
+    case "medio": segundos = 6;
+    break;
+    case "dificil": segundos = 3;
+    default : segundos =6;
+
+}
+
+console.log(segundos);
